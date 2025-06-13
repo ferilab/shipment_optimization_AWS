34de827dae2c84fece6c30ede19cc00004596c2c
Fereidoun Mianji <fmianji@gmail.com>
2025-06-12 22:48:24 -0400

    Cloud run test

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -69,9 +69,7 @@ def recommend_top_k_configs(context_dict, model, encoder, opt_num=3):
     model_input = pd.concat([df.drop(columns=cat_cols), encoded_df], axis=1)
     df['Predicted_Delivery_Time'] = model.predict(model_input)
 
-    st.write(df.columns)
-    #st.write(df.sort_values("Predicted_Delivery_Time").head(3))  # to debug the contents of df
-
+    # st.write(df.columns)
     return df.sort_values("Predicted_Delivery_Time").head(opt_num)
 
 # --- Button Trigger ---
